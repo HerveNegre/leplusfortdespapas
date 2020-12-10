b2e7ea9080dfde08af363371754157a0efffaf1e
Brice <brice.verriere@epitech.eu>
2020-12-10 16:56:32 +0100

    maj 10 12 2020

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,219 @@
-@extends('layouts.master')
 
-@section('content')
 
-<!-- 
-    
-ESPACE POUR UN CARROUSEL / IMAGE DE FOND
+<!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
 
--->
-<br><br><br><br><br>
-
-    <h1>Page d'accueil</h1>
-    <div class="container">
-        <div class="row">
-            @foreach ($products as $product)
-                <div class="col-lg-3 col-md-6">
-                    <div class="single-product">
-                        <img class="img-fluid" src="images/p6.jpg" alt="chaussure de sport rouge">
-                        <div class="product-details">
-                            <h6>{{ $product->name }}</h6>
-                            <p>{{ $product->details }}</p>
-                            <div class="price">
-                                <h6>{{ $product->price }}</h6>
-                            </div>
-                        </div>
+        <!-- header section start -->
+    -->
+    <!-- header section end -->
+    @extends('layouts.master')
+    <!-- pages-title-start -->
+    @section('content')
+    <br><br><br><br><br>
+    <h1 class="homeMainTitle">Bienvenue sur LePlusFortdesPapas !</h1>
+    <br>
+    <h2 class="homeMainTitle">Pour des papas, par des papas</h2>
+    <br><br><br><br><br>
+    <div class="pages-title section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="pages-title-text text-center">
+                        <h2>Voici des conseils pour les jeunes papas afin d'appréhender avec sérénité les petits tracas, que va vivre votre bébé</h2>
+                        <!-- <ul class="text-left">
+                            <li><a href="index.html">Home </a></li> -->
+                            <!-- <li><span> // </span><a href="blog.html">blog </a></li>
+                            <li><span> // </span>fashion style fine arts drawing</li> -->
+                        <!-- </ul> -->
                     </div>
                 </div>
-            @endforeach
+            </div>
         </div>
     </div>
+    <br><br>
+    <!-- pages-title-end -->
+    <!-- blog-section-start -->
+    <section class="pages blog single-blog-area section-padding-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-md-9">
+                    <div class="single-blog-page">
+                        <div class="single-blog-img">
+                            <img src="img/blog/s.jpg" alt="" />
+                        </div>
+                        <div class="padding30">
+                            <div class="blog-text">
+                                <div class="post-title">
+                                    <h3>Les coliques (pleurs intenses)</h3>
+                                    <ul class="clearfix">
+                                        <li><i class="pe-7s-user"></i>By :<a href="#">Valérie de Beaumont, infirmière bachelière et IBCLC</a><span>|</span></li>
+                                        <li><i class="pe-7s-comment"></i><a href="#">Avril 2017 </a><span>|</span></li>
+                                        <li><i class="pe-7s-like"></i><a href="#">210 Like</a><span>|</span></li>
+                                        <li><i class="pe-7s-back"></i><a href="#">69 Comments</a></li>
+                                    </ul>
+                                </div>
+                                <p> <strong>Q’est-ce que les coliques?</strong></p>
+                                <div class="italic">
+                                    <p>En général, les coliques sont décrites comme des crises de pleurs récurrentes et régulières chez un bébé. Elles ont lieu au moins 3 heures par jour, 3 jours par semaine, pendant plus d’une semaine. Les crises surviennent en général aux mêmes moments de la journée, le plus souvent en fin d’après-midi ou en soirée. 
+                                        <br>Les coliques sont assez fréquentes chez les jeunes bébés : jusqu’à 20 % des nourrissons seraient touchés. Selon certaines études, les bébés allaités exclusivement seraient moins touchés que les bébés nourris avec des préparations commerciales à base de lait de vache.
+                                        <br>Les coliques peuvent commencer 3 semaines après la naissance, mais surviennent plus souvent entre la 6e et la 8e semaine. Elles disparaissent graduellement vers le 4e mois.
+                                        <br></p>
+                                </div>
+                                <p><ul>Lorsqu’un bébé fait des coliques, les pleurs peuvent être accompagnés de certains comportements caractéristiques. Le bébé :
+                                    <li>- serre les poings et devient rouge;</li>
+                                    <li>- raidit les jambes et les muscles de son ventre. Il peut aussi replier les jambes contre son ventre;</li>
+                                    <li>- a le ventre dur et a souvent des gaz pendant une crise;</li>
+                                    <li>- peut aussi arquer le dos.</li>
+                                </ul></p>
+                                <div class="apparte">
+                                    <p>Les coliques ne sont pas une maladie, mais plutôt la description d’un comportement.<br>
+                                        Les seuls et uniques réels symptômes toujours associés aux coliques sont d’ailleurs les pleurs intenses et soutenus.</p>
+                                    </div><br><br>
+                                    <p>Par ailleurs, un bébé qui a des coliques est un enfant en bonne santé. Il a un réflexe de succion normal. Il boit bien. Il peut régurgiter à l’occasion, mais ne vomit pas. Il ne fait pas de fièvre. Il n’est pas léthargique. Ses selles sont normales et il mouille 6 à 8 couches par jour. De plus, il a toujours un gain de poids satisfaisant, c’est-à-dire qu’il suit sa courbe de croissance. </p>
+                                    <br>
+                                    <h3>Quelles sont les causes des coliques?</h3>
+                                    <p>La cause des coliques demeure souvent un mystère.
+                                        <br>Elle ne peut être clairement déterminée que dans 5 % des cas. Plusieurs hypothèses existent toutefois pour tenter d’en expliquer l’origine.</p>
+                                    <br>
+                                    <h3>Comment soulager les coliques de bébé?</h3>
+                                    <p><ul>
+                                        <li>Garder l’environnement très calme autour du bébé et baisser les lumières;</li>
+                                        <li>Bercer le bébé doucement;</li>
+                                        <li>Porter le bébé dans un porte-bébé ou une écharpe;</li>
+                                        <li>Faire un massage abdominal en frottant dans le sens des aiguilles d’une montre;</li>
+                                        <li>Éviter la suralimentation (trop de biberons ou de tétées);</li>
+                                        <li>Limiter l’absorption d’air durant la tétée ou le biberon et essayer de bien faire faire le rot;</li>
+                                        <li>Pour les bébés allaités, ne donner qu’un seul sein par tétée ou le même sein pour 2 tétées de suite de façon à ce que bébé atteigne le lait gras de fin de tétée qui peut prévenir les coliques. </li>
+                                    </ul></p>
+                                    <div class="apparte">
+                                        <p>
+                                            Dans une telle situation, il est normal de ressentir de la colère. <br>La colère se manifeste en effet dans des situations imprévisibles et incontrôlables. <br>Cette émotion n’a rien à voir avec le fait d’être un mauvais parent. <br>Il est toutefois important de ne pas s’emporter. 
+                                        </p>
+                                    </div>
+                                        <br><br><br>
+                                    <div class="share-tag clearfix">
+                                    <ul class="blog-share floatleft">
+                                        <li><h5>--- </h5></li>
+                                        <li><a href="#"><i class="mdi mdi-facebook"></i></a></li>
+                                        <li><a href="#"><i class="mdi mdi-twitter"></i></a></li>
+                                        <li><a href="#"><i class="mdi mdi-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="mdi mdi-vimeo"></i></a></li>
+                                        <li><a href="#"><i class="mdi mdi-dribbble"></i></a></li>
+                                        <li><a href="#"><i class="mdi mdi-instagram"></i></a></li>
+                                    </ul>
+                                </div>
+                                
+                                
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!-- single post end -->
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <!-- blog section end -->
+    <!-- related post section start -->
+    <section class="related-post latest-blog section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="section-title text-center">
+                        <h2>Articles liés</h2>
+                    </div>
+                </div>
+            </div>
+            <ul class="blog-row">
+                <li>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="l-blog-text">
+                                <div class="banner"><a href="single-blog.html"><img src="img/blog/4.jpg" alt="" /></a></div>
+                                <div class="s-blog-text">
+                                    <h4><a href="single-blog.html">La poussée dentaire</a></h4>
+                                    <span>By : <a href="#">Dre Thao Phan, dentiste pédiatrique</a> | <a href="#">210 Like</a> | <a href="#">69 Comments</a></span>
+                                    h3>Les symptômes</h3>
+                                                <p><ul>Votre enfant : 
+                                                    <li>- salive beaucoup et a tendance à mordiller;</li>
+                                                    <li>- est irritable et pleure facilement;</li>
+                                                    <li>- a les joues rouges;</li>
+                                                    <li>- mange et dort moins bien que d’habitude;</li>
+                                                    <li>- a les gencives un peu gonflées;</li>
+                                                    <li>- a une petite bulle bleue sur la gencive (un « kyste d’éruption »)
+                                                        <br>annonçant la percée prochaine d’une dent. </li>
+                                                    </ul></p>
+                                                    <strong>Normalement, les poussées dentaires ne provoquent pas de complications.
+                                                        <br>Si votre enfant présente d’autres symptômes, il souffre probablement d’autre chose.
+                                                        <br>En cas de fièvre élevée, la poussée dentaire est rarement en cause.</strong>
+                                                    <br>
+                                                    <br>
+                                                    <h3>Qu’est-ce que les poussées dentaires?</h3>
+                                                    <p>Chez certains enfants, l’apparition des dents de lait (aussi appelées « dents primaires » ou « dents de bébé »)
+                                                        <br>passe presque inaperçue, tandis que chez d’autres elle s’accompagne de malaises ou d’inconforts. </p>
+                                                        <p><strong>Âge des premières dents : </strong>
+                                                        <br>Les premières dents percent généralement vers l’âge de 6 mois.
+                                                        <br>Elles peuvent toutefois apparaître plus tôt. Certains enfants naissent même avec une ou deux dents.
+                                                        <br>Dans d’autres cas, les premières dents ne sortent qu’à l’âge de 14 mois.</p>
+                                                        <p><ul>L’âge d’apparition des dents est très variable. En général, elles sortent toutefois dans l’ordre suivant :
+                                                            <li>- les deux incisives du bas, puis celles du haut (entre 6 et 12 mois); </li>
+                                                            <li>- les incisives latérales (entre 9 et 13 mois);</li>
+                                                            <li>- les premières molaires (entre 13 et 19 mois);</li>
+                                                            <li>- les canines (entre 16 et 22 mois);</li>
+                                                            <li>- enfin, les deuxièmes molaires (entre 25 et 33 mois).</li>
+                                                        </ul></p>
+                                                        <strong>Combien de dents de bébé en tout?</strong>
+                                                        <p>Dans la majorité des cas, à 3 ans, l’enfant a ses 20 dents primaires. </p>
+                                                        <p>Pendant ce temps, les 32 dents d’adulte se développent dans les mâchoires. Elles apparaissent entre 6 et 12 ans environ.
+                                                            <br>Quant aux dents de sagesse, elles percent généralement plus tard. Certaines personnes n’ont toutefois pas de dents de sagesse.</p>
+                                                        <br>
+                                                        <h3>Comment traiter?</h3>
+                                                        <h3>Soins et conseils pratiques</h3>
+                                                        <p>Les poussées de dents ne nécessitent pas de traitements. Il n’y a rien à faire à part soulager l’inconfort.</p>
+                                                        <p>N’hésitez surtout pas à réconforter et à cajoler votre enfant quand une de ses dents perce.
+                                                            <br>Les quelques soins qui suivent vous aideront à soulager son malaise ou son inconfort. </p>
+                                                        <p><ul>
+                                                            <li>- <strong>Frottez-lui doucement les gencives</strong> avec une débarbouillette imbibée d’eau froide enroulée autour de votre doigt.
+                                                                <br>Assurez-vous toutefois de bien laver vos mains d’abord.
+                                                                <br>Vous pouvez laisser votre bébé mâchouiller la débarbouillette sous votre supervision.
+                                                                <br>Vous pouvez aussi lui masser les gencives très doucement avec une brosse à dents pour bébé aux soies très souples.
+                                                                <br>Utilisez-la avec beaucoup de délicatesse. </li>
+                                                                <br>
+                                                                <li>- <strong>Donnez-lui un anneau de dentition en caoutchouc.</strong>Choisissez-en un qui est incassable, qui ne s’effrite pas 
+                                                                    <br>et qui est assez gros pour ne pas pouvoir pénétrer entièrement dans la bouche de votre bébé.
+                                                                    <br>Idéalement, sa surface doit être texturée.
+                                                                    <br>Évitez les anneaux en PVC ou ceux qui renferment du liquide.
+                                                                    <br>On peut réfrigérer un anneau de dentition, mais on ne doit pas le congeler.
+                                                                    <br>Il ne faut pas non plus l’attacher avec un ruban autour du cou de l’enfant. </li>
+                                                                <br>
+                                                                <li>- <strong>Au besoin, vous pouvez donner de l’acétaminophène</strong> à votre bébé. </li>
+                                                        </ul></p>
+                                                        
+                                                        <div class="apparte">
+                                                        <p><ul><strong>À éviter : le collier de noisetier, les légumes crus et le biscuit de dentition</strong>
+                                                            <li>- S’il est très à la mode dans certains milieux, le collier de noisetier est déconseillé par les autorités médicales, car le bébé risque de s’étouffer si des billes se détachent.</li>
+                                                            <li>- Ne donnez pas non plus à votre enfant des morceaux de légumes ou de fruits crus « durs » (pomme, carotte, céleri, etc.) avant l’âge de 2 ans afin de prévenir les risques d’étouffement. </li>
+                                                            <li>- Les biscuits de dentition sont également déconseillés en raison du sucre qu’ils contiennent qui cause la carie dentaire.</li>
+                                                            <li>- Les gels analgésiques topiques sont dangereux, car ils agissent de façon superficielle et risquent de nuire à la capacité du bébé d’avaler.</li>    
+                                                        </ul></p>
+                                                    </div>
+                                                    <br><br>
+                                    </div>
+                                </div>
+                                <div class="date-read clearfix">
+                                    <a href="#"><i class="mdi mdi-clock"></i> Novembre 2017 </a>
+                                    <a href="single-blog.html"></a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    
 @endsection
